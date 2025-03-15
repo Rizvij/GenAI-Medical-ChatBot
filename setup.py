3812c9660df98eb6055af377d8c2aef8a129a61b
mohtashem <mohtashem.rizvi@gmail.com>
2025-03-15 14:34:17 +0530

    add folder structure

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Medical-Chatbot',
+    version= '0.0.0',
+    author= 'Junaid Rizvi',
+    author_email= 'rizvi.junaidjmi@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
